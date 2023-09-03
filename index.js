340d273da3ab0ed082ead2b63f8b617d3c4b3067
John Francis <john9francis@gmail.com>
2023-09-02 21:46:11 -0600

    Created thing for the client to be able to change

--- index.js
+++
@@ -1,7 +0,0 @@
-const Express = require("express")()
-const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
-
-Http.listen(3000, () => {
-  console.log("Listening at :3000...")
-})
--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const Express = require("express")()
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http);
 
+var thing = 1 // a thing for the clients to change
+
+Socketio.on("connection", socket => {
+  socket.emit("thing", thing)
+})
+
 Http.listen(3000, () => {
   console.log("Listening at :3000...")
 })
